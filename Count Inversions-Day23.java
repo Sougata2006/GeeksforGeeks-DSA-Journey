5aee18c5b3c3563fd626c9adb0fca90bd05612d6
Sougata Paul <psougata32@gmail.com>
2024-12-24 23:36:37 +0530

    Update Count Inversions-Day23.java

--- Count Inversions-Day23.java
+++ Count Inversions-Day23.java
@@ -1,49 +1,18 @@
 class Solution {
-    public int inversionCount(int arr[], int l, int r) {
-        int count = 0;
-        if (l < r) {
-            int m = (l + r) / 2;
 
-            count += inversionCount(arr, l, m);
+    static int inversionCount(int arr[]) {
+        int n = arr.length; 
+        int invCount = 0;  
 
-            count += inversionCount(arr, m + 1, r);
-
-            count += mergeAndCount(arr, l, m, r);
-        }
-        return count;
-    }
-
-    public int mergeAndCount(int arr[], int l, int m, int r) {
-        int n1 = m - l + 1; 
-        int n2 = r - m;   
-
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-            left[i] = arr[l + i];
-        for (int j = 0; j < n2; j++)
-            right[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0, k = l, res = 0;
-
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
-            } else {
-                arr[k++] = right[j++];
-                res += (n1 - i); 
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+              
+            
+                if (arr[i] > arr[j])
+                    invCount++;
             }
         }
-
-        while (i < n1)
-            arr[k++] = left[i++];
-
-        while (j < n2)
-            arr[k++] = right[j++];
-
-        return res;
+        return invCount; 
     }
-
 
 }
