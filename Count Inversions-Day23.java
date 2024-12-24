a2b3a899f3d8a67387ed42fbba2c0c203ddcf693
Sougata Paul <psougata32@gmail.com>
2024-12-24 23:40:28 +0530

    Update Count Inversions-Day23.java

--- Count Inversions-Day23.java
+++ Count Inversions-Day23.java
@@ -1,18 +1,50 @@
 class Solution {
 
-    static int inversionCount(int arr[]) {
-        int n = arr.length; 
-        int invCount = 0;  
+    static int mergeAndCount(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-              
-            
-                if (arr[i] > arr[j])
-                    invCount++;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left, invCount = 0;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                invCount += (n1 - i); 
             }
         }
-        return invCount; 
+
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+
+        return invCount;
     }
 
+    static int mergeSortAndCount(int[] arr, int left, int right) {
+        int invCount = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            invCount += mergeSortAndCount(arr, left, mid);
+            invCount += mergeSortAndCount(arr, mid + 1, right);
+            invCount += mergeAndCount(arr, left, mid, right);
+        }
+        return invCount;
+    }
+
+    static int inversionCount(int[] arr) {
+        return mergeSortAndCount(arr, 0, arr.length - 1);
+    }
 }
