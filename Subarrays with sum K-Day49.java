317f509abe82a0f8ef1a38f151cb85674894edac
Sougata Paul <psougata32@gmail.com>
2025-01-19 21:45:24 +0530

    Create Subarrays with sum K-Day49.java

---
+++ Subarrays with sum K-Day49.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSubarrays(int arr[], int k) {
+        
+        Map <Integer , Integer> prefixSums = new HashMap <>();
+        int res =0;
+        int currSum = 0;
+        
+        for(int i =0;i<arr.length;i++){
+            currSum += arr[i];
+            
+            if(currSum == k)
+            res++;
+            
+            if(prefixSums.containsKey(currSum - k))
+            res += prefixSums.get(currSum - k);
+            
+            prefixSums.put(currSum , prefixSums.getOrDefault(currSum,0)+1);
+        }
+        return res;
+    }
+}
