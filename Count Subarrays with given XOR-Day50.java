b46108afb5e2a2884e91e70995a2a643a95df01e
Sougata Paul <psougata32@gmail.com>
2025-01-20 22:51:55 +0530

    Create Count Subarrays with given XOR-Day50.java

---
+++ Count Subarrays with given XOR-Day50.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        
+        int res = 0;
+        
+        HashMap <Integer,Integer> mp = new HashMap<>();
+        
+        int prefXOR =0;
+        
+        for(int val : arr){
+            prefXOR ^= val;
+            res += mp.getOrDefault(prefXOR ^ k,0);
+            
+            if(prefXOR == k)
+            res++;
+            
+            mp.put(prefXOR, mp.getOrDefault(prefXOR , 0)+1);
+        }
+        return res;
+    }
+}
