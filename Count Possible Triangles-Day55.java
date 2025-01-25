67f66fa83578d170d1f2452edb8686fe91cef6dc
Sougata Paul <psougata32@gmail.com>
2025-01-25 21:53:54 +0530

    Create Count Possible Triangles-Day55.java

---
+++ Count Possible Triangles-Day55.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class GfG {
+  
+    static int countTriangles(int[] arr) {
+        int res = 0;
+        Arrays.sort(arr);
+
+        for (int i = 2; i < arr.length; ++i) {
+         
+            int left = 0, right = i - 1;
+
+            while (left < right) {
+                if (arr[left] + arr[right] > arr[i]) {
+                  
+                    res += right - left;
+
+                    right--;
+                } else {
+               
+                    left++;
+                }
+            }
+        }
+
+        return res;
+    }
+
