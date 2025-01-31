15df7437778b52b2b549d58ff034e4df1191fdce
Sougata Paul <psougata32@gmail.com>
2025-01-31 23:23:38 +0530

    Create Equilibrium Point-Day61.java

---
+++ Equilibrium Point-Day61.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public static int findEquilibrium(int arr[]) {
+
+    int prefSum = 0;
+    int total =0;
+        
+    for (int ele :arr){
+        total += ele;
+    }
+        for(int pivot = 0; pivot<arr.length; pivot++){
+                
+            int suffSum = total-prefSum - arr[pivot];
+            if(prefSum == suffSum){
+                return pivot;
+                
+            }
+            prefSum += arr[pivot];
+        }
+           return -1;
+    }
+}
