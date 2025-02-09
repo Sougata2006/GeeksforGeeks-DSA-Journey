3b1c95e38f70efaad4baff36ce34179b26dc6657
Sougata Paul <psougata32@gmail.com>
2025-02-09 20:38:44 +0530

    Create Clone List with Next and Random-Day70.java

---
+++ Clone List with Next and Random-Day70.java
@@ -0,0 +1,42 @@
+class Solution {
+    public Node cloneLinkedList(Node head) {
+
+        if(head == null){
+        return null;
+        }
+        
+        Node curr = head;
+        while(curr != null){
+            Node newNode = new Node(curr.data);
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next;
+        }
+        curr = head;
+        while(curr != null){
+            if(curr.random != null){
+                curr.next.random = curr.random.next;
+            }
+        
+        curr = curr.next.next;
+    }
+    curr = head;
+    Node clonedHead = head.next;
+    Node clone = clonedHead;
+    while(clone.next != null){
+        
+        curr.next = curr.next.next;
+        clone.next = clone.next.next;
+        
+        curr = curr.next;
+        clone = clone.next;
+    }
+    curr.next = null;
+    clone.next = null;
+    
+    return clonedHead;
+    
+    
+    
+    }
+}
