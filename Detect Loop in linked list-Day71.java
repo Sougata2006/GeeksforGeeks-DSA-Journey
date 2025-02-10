7c546e3502b9f83ab4c2fc7fc3f5862c4baa5303
Sougata Paul <psougata32@gmail.com>
2025-02-10 12:48:37 +0530

    Create Detect Loop in linked list-Day71.java

---
+++ Detect Loop in linked list-Day71.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean detectLoop(Node head) {
+
+        Node slow = head , fast = head;
+        
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
