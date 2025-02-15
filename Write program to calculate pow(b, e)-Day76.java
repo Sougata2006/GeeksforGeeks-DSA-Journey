320ca7e271cdf85cbb4bf28255ef86aadfe639be
Sougata Paul <psougata32@gmail.com>
2025-02-15 23:06:14 +0530

    Create Write program to calculate pow(b, e)-Day76.java

---
+++ Write program to calculate pow(b, e)-Day76.java
@@ -0,0 +1,12 @@
+
+class GFG {
+    static double power(double b, int e) {
+        return Math.pow(b, e);
+    }
+
+    public static void main(String[] args) {
+        double b = 3.0;
+        int e = 5;
+        System.out.println(power(b, e));
+    }
+}
