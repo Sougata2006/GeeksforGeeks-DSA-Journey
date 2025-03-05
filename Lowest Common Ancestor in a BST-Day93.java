afe42408533985f8b84ca9e775760d6859a1bb3a
Sougata Paul <psougata32@gmail.com>
2025-03-05 23:29:16 +0530

    Create Lowest Common Ancestor in a BST-Day93.java

---
+++ Lowest Common Ancestor in a BST-Day93.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        
+        while(root != null){
+            if(root.data>n1.data && root.data>n2.data)
+            root = root.left;
+            
+            else if(root.data<n1.data && root.data<n2.data)
+            {
+             root = root.right;
+            }
+            else{
+                break;
+            }
+        }
+        return root;
+    }
+}
